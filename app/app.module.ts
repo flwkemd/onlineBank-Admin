e2aed55bd234b037c31a1f1165103ac9817f9872
flwkemd <flwkemd@naver.com>
2018-01-29 02:32:24 +0100

    implemented login service

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,19 +5,24 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
+import { LoginService } from './login.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
